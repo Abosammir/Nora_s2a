2e1b165bdba901ca993af390e2ebb3abcd0a29ab
Abosamir <m.samir00a@gmail.com>
2021-09-02 02:54:18 +0200

    ..

---
+++ app/src/main/java/com/example/bookapp/activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.bookapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bookapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
